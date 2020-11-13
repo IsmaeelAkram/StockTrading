48d9878ae7a2e90d6bb51a9c7d7053af001fc922
Ismaeel Akram <ismaeelakram01@gmail.com>
2020-11-12 22:20:14 -0500

    Init browser

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ load_dotenv()
 import robin_stocks as robinhood
 import os
 import yahoo_finance
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-account = robinhood.login(os.getenv('ROBINHOOD_USER'), os.getenv('ROBINHOOD_PASS'))
+# Login to Robinhood
+# account = robinhood.login(os.getenv('ROBINHOOD_USER'), os.getenv('ROBINHOOD_PASS'))
 
+# Setup selenium browser
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+browser = webdriver.Chrome(options=chrome_options)
+
+# Get most active stocks
+browser.get("https://finance.yahoo.com/most-active")
