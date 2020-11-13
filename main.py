22c9eab87e37584e318157509c348d9556fa7ee2
Ismaeel Akram <ismaeelakram01@gmail.com>
2020-11-13 00:16:08 -0500

    Get cheapest of active stocks

--- main.py
+++ main.py
@@ -2,24 +2,18 @@ from dotenv import load_dotenv
 load_dotenv()
 import robin_stocks as robinhood
 import os
+import sys
 import yahoo_finance
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import chalk
 
-def get_active_stocks(browser):
-    print(chalk.green("Getting new active stocks..."))
-    browser.get("https://finance.yahoo.com/most-active")
-    active_stocks_symbols = browser.find_elements_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr/td[1]/a')
-    active_stocks_names = browser.find_elements_by_xpath('/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[6]/div/div/section/div/div[2]/div[1]/table/tbody/tr/td[2]')
-    active_stocks_prices = browser.find_elements_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr/td[3]/span')
-    return dict(zip(active_stocks_symbols, zip(active_stocks_names, active_stocks_prices)))
-    # symbol = stock.text
-    # name = active_stocks[stock][0].text
-    # price = active_stocks[stock][1].text
+from stock import Stock
+from helpers import get_active_stocks
 
 # Login to Robinhood
+# print(chalk.green("Logging into Robinhood..."))
 # account = robinhood.login(os.getenv('ROBINHOOD_USER'), os.getenv('ROBINHOOD_PASS'))
 
 # Setup selenium browser
@@ -27,10 +21,23 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 browser = webdriver.Chrome(options=chrome_options)
 
+
 # Get active stocks
-active_stocks = get_active_stocks(browser)
-for stock in active_stocks:
-    symbol = stock.text
-    name = active_stocks[stock][0].text
-    price = active_stocks[stock][1].text
-    print(f"{chalk.green(symbol)} ({chalk.yellow(name)}): ${chalk.green(price)}")
+stocks = get_active_stocks(browser)
+
+# Get cheapest stock
+print(chalk.green("Getting cheapest of active stocks..."))
+stock_prices = []
+for stock in stocks:
+    stock_prices.append(stock.price)
+min_stock_price = min(stock_prices)
+
+cheapest_stock = None
+for stock in stocks:
+    if(min_stock_price is stock.price):
+        cheapest_stock = stock
+if(cheapest_stock is None):
+    print(chalk.red("Could not find cheapest of active stocks!"))
+    sys.exit(1)
+else:
+    print(chalk.green("Cheapest stock is: ") + f"{cheapest_stock.symbol} ({cheapest_stock.name}) (${cheapest_stock.price})")
